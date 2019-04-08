d8552b79c0416ebc23b1edfd87bf0f2f652e665d
Zettex <fedor-14let@mail.ru>
2019-04-08 21:51:26 +0500

    Add firebase config

---
+++ src/firebase/firebaseConfig.js
@@ -0,0 +1,8 @@
+export default { 
+  apiKey: "AIzaSyBPGqvZe5ZH0YZM5SBYyiDY-zAVIiIBukc",
+  authDomain: "chat-app-31bf2.firebaseapp.com",
+  databaseURL: "https://chat-app-31bf2.firebaseio.com",
+  projectId: "chat-app-31bf2",
+  storageBucket: "chat-app-31bf2.appspot.com",
+  messagingSenderId: "929096246188"
+}
